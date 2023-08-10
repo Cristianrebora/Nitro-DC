03e87900734ae69ee9defe57853e87d67a610072
Cristian Rebora <cristian.rebora.prog@gmail.com>
2023-08-09 22:25:43 -0300

    proyecto nuevo js falta detalles

---
+++ js/productos.js
@@ -0,0 +1,170 @@
+const productos = [
+	{
+		id: 1,
+		nombre: 'Tabla Nitro 1.49',
+		precio: 800000,
+		img: 'sss.webp'
+	},
+	{
+		id: 2,
+		nombre: 'Tabla Nitro 160 wide',
+		precio: 500000,
+		img: 'nitro2.webp'
+	},
+	{
+		id: 3,
+		nombre: 'Tabla nitro 1.55',
+		precio: 700000,
+		img: 'nitro3.webp'
+	},
+	{
+		id: 4,
+		nombre: 'Fijaciones Reazen',
+		precio: 350000,
+		img: 'fijacionesnitro.webp'
+	},
+	{
+		id: 5,
+		nombre: 'Fijaciones Peison ',
+		precio: 4500000,
+		img: 'fijacionesnitro2.webp'
+	},
+	{
+		id: 6,
+		nombre: 'Fijaciones Classic',
+		precio: 250000,
+		img: 'fijacionesnitro1.webp'
+	},
+	{
+		id: 7,
+		nombre: 'Botas Snowboard  ',
+		precio: 250000,
+		img: 'nitrobotas.webp'
+	},
+	{
+		id: 8,
+		nombre: 'Botas Mujer ',
+		precio: 375000,
+		img: 'nitrobotasmujer.webp'
+	},
+	{
+		id: 9,
+		nombre: 'Botas Venture',
+		precio: 460000,
+		img: 'botasventurenitro.webp'
+	},
+	{
+		id: 10,
+		nombre: 'Funda Snow 1.55',
+		precio: 98000,
+		img: 'funda1.webp'
+	},
+	{
+		id: 11,
+		nombre: 'Funda Snow 1.49',
+		precio: 98000,
+		img: 'funda2.webp'
+	},
+	{
+		id: 12,
+		nombre: 'Funda Snow Space Sack',
+		precio: 139.900,
+		img: 'funda3.webp'
+	},
+	{
+		id: 13,
+		nombre: 'Mochila Tactica Nitro',
+		precio: 60000,
+		img: 'mochitacticanitro.webp'
+	},
+	{
+		id: 14,
+		nombre: 'Mochila T. Jazards ',
+		precio: 65000,
+		img: 'mochitacticajazardsnitro.webp'
+	},
+	{
+		id: 15,
+		nombre: 'Mochila Nitro Camuflada',
+		precio: 80000,
+		img: 'mochitacticacamuflada.webp'
+	},
+	{
+		id: 16,
+		nombre: 'Campera Niño DC',
+		precio: 70000,
+		img: 'camperanino.webp'
+	},
+	{
+		id: 17,
+		nombre: 'Campera Mujer DC',
+		precio: 110000,
+		img: 'camperamujer.webp'
+	},
+	{
+		id: 18,
+		nombre: 'Campera Hombre DC',
+		precio: 150000,
+		img: 'camperahombre.webp'
+	},
+	{
+		id: 19,
+		nombre: 'Antiparra Gadnic',
+		precio: 40000,
+		img: 'antiparragadnic.webp'
+	},
+	{
+		id: 20,
+		nombre: 'Antiparra Oakley',
+		precio: 50000,
+		img: 'antiparraoakley1.webp'
+	},
+	{
+		id: 21,
+		nombre: 'Antiparra OaKley Pro',
+		precio: 98000,
+		img: 'antiparraoaKley2.webp'
+	},
+	{
+		id: 22,
+		nombre: 'Gorro Nieve DC',
+		precio: 7500,
+		img: 'gorro1.webp'
+	},
+	{
+		id: 23,
+		nombre: 'Gorro DC Snow/Ski',
+		precio: 8500,
+		img: 'gorro2.webp'
+	},
+	{
+		id: 24,
+		nombre: 'Gorro DC Nieve Mujer',
+		precio: 10000,
+		img: 'gorro3.webp'
+	},
+	{
+		id: 25,
+		nombre: 'Cuello Termico DC',
+		precio: 6000,
+		img: 'cuellotermico1.webp'
+	},
+	{
+		id: 26,
+		nombre: 'Cuello Termico DC',
+		precio: 4500,
+		img: 'cuellotermico2.webp'
+	},
+	{
+		id: 27,
+		nombre: 'Cuello Termico DC',
+		precio: 5000,
+		img: 'cuellotermico3.webp'
+	},
+	{
+		id: 28,
+		nombre: 'Remera Termica Nitro',
+		precio: 16000,
+		img: 'remeratermicanitro.webp'
+	}
+];
